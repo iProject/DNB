390db8bfb134303e53ae730464107410bfead412
DorothyKwarteng <kwartengdorothy3@gmail.com>
2019-08-03 19:12:17 +0000

    this is deadly welcome page

---
+++ app/src/main/java/com/example/dnb/DnbWelcome.java
@@ -0,0 +1,13 @@
+package com.example.dnb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DnbWelcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dnb_welcome);
+    }
+}
--- app/src/main/java/com/example/dnb/MainActivity.java
+++ app/src/main/java/com/example/dnb/MainActivity.java
@@ -1,13 +1,44 @@
 package com.example.dnb;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    Button signIn;
+    Button logIn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        logIn = findViewById(R.id.btn_login);
+        signIn = findViewById(R.id.btn);
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //moving to the next page
+                Intent nextPage;
+                nextPage = new Intent(MainActivity.this, DnbWelcome.class);
+
+                startActivity(nextPage);
+
+            }
+        }  );
+        logIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                // moving to the next page
+                Intent nextPage;
+                nextPage = new Intent(MainActivity.this, DnbWelcome.class);
+
+                startActivity(nextPage);
+
+            }
+        });
+
     }
 }
